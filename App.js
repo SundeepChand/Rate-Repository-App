784d51e584293dce2a102340fa3525d2b427ad32
Sundeep Chand <sundeep.chand2001@gmail.com>
2020-10-23 19:18:12 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello Sundeep Chand!!!?</Text>
+
+      <TouchableWithoutFeedback
+        onPress={() => Alert.alert("You pressed the text")}
+      >
+        <Text>Press Me!</Text>
+      </TouchableWithoutFeedback>
       <StatusBar style="auto" />
     </View>
   );
