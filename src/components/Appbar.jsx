e82564c618199280355f396f8d0f9053a487cc6d
Sundeep Chand <sundeep.chand2001@gmail.com>
2020-10-24 20:50:28 +0530

    Finished upto 10.7

--- src/components/Appbar.jsx
+++ src/components/Appbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, StyleSheet, TouchableWithoutFeedback } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import { Link } from 'react-router-native';
 
 import Text from './Text';
 import theme from '../../themes';
@@ -8,17 +9,28 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: theme.colors.primary,
     padding: 20,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+  text: {
+    marginRight: 10,
   },
 });
 
-const AppBar = ({ children }) => {
+const generateRoutes = (routes) => routes.map(({ route, name }, index) => (
+  <Link to={route} key={index} component={TouchableOpacity}>
+    <Text style={styles.text} color="textPrimary" fontSize="subheading" fontWeight="bold">
+      {name}
+    </Text>
+  </Link>
+));
+
+const AppBar = ({ routes }) => {
   return (
     <View style={styles.container}>
-      <TouchableWithoutFeedback>
-        <Text color="textPrimary" fontSize="subheading" fontWeight="bold">
-          {children}
-        </Text>
-      </TouchableWithoutFeedback>
+      <ScrollView horizontal>
+        { generateRoutes(routes) }
+      </ScrollView>
     </View>
   );
 };
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Constants from 'expo-constants';
 import { StyleSheet, View } from 'react-native';
+import { Route, Switch, Redirect } from 'react-router-native';
 
 import AppBar from './Appbar';
 import RepositoryList from './RepositoryList';
 import theme from '../../themes';
+import SignIn from './SignIn';
 
 const styles = StyleSheet.create({
   container: {
@@ -15,11 +17,29 @@ const styles = StyleSheet.create({
   },
 });
 
+const routes = [
+  {
+    name: "Repositories",
+    route: "/"
+  },{
+    name: "Sign In",
+    route: "/signin"
+  },
+];
+
 const Main = () => {
   return (
     <View style={styles.container}>
-      <AppBar>Repositories</AppBar>
-      <RepositoryList />
+      <AppBar routes={routes} />
+      <Switch>
+        <Route path="/" exact>
+          <RepositoryList />
+        </Route>
+        <Route path="/signin">
+          <SignIn />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
     </View>
   );
 };
